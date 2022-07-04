677dd96317bb7d1ccaef9e40163065c7988c9562
bai_777 <bai_777@mail.ru>
2022-07-04 18:45:08 +0300

    реализовал обработчики кнопок Поехали и выход

--- app/src/main/java/com/example/mosquitonetcalculator/ui/StartFragment.kt
+++ app/src/main/java/com/example/mosquitonetcalculator/ui/StartFragment.kt
@@ -7,9 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.mosquitonetcalculator.R
 import com.example.mosquitonetcalculator.databinding.FragmentStartBinding
+import kotlin.system.exitProcess
 
 
-class StartFragment : Fragment() {
+class StartFragment : Fragment(), View.OnClickListener {
 
     private var _binding: FragmentStartBinding? = null
     private val binding: FragmentStartBinding get() = _binding!!
@@ -17,9 +18,25 @@ class StartFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentStartBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonStart.setOnClickListener(this)
+        binding.buttonExit.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.buttonStart->{
+                MAIN.navController.navigate(R.id.action_startFragment_to_mainFragment)
+            }
+            R.id.buttonExit->{
+                exitProcess(0)
+            }
+        }
+    }
 }
