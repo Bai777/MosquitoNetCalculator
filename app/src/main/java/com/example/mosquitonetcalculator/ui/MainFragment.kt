e8538273f315d0bc39d705e112fbb3753b1ae47c
bai_777 <bai_777@mail.ru>
2022-07-04 19:52:18 +0300

    добавил меню на главный экран

--- app/src/main/java/com/example/mosquitonetcalculator/ui/MainFragment.kt
+++ app/src/main/java/com/example/mosquitonetcalculator/ui/MainFragment.kt
@@ -5,17 +5,44 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.mosquitonetcalculator.R
+import com.example.mosquitonetcalculator.databinding.FragmentMainBinding
 
 
 class MainFragment : Fragment() {
 
+    private var _binding: FragmentMainBinding? = null
+    private val binding: FragmentMainBinding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View {
+        _binding = FragmentMainBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        workWhitItemMenuInToolbar()
+    }
+
+    private fun workWhitItemMenuInToolbar() {
+        binding.leftTopAppBarMainScreenFragment.inflateMenu(R.menu.menu_main_screen_add)
+        binding.leftTopAppBarMainScreenFragment.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.action_add -> {
+                    Toast.makeText(requireContext(), "Menu", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
